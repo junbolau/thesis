e6402021363dd887e47ade80f73dae2a1b2d0a10
Jun <laujunbo@gmail.com>
2023-05-29 22:12:53 -0700

    Edited some parts of Chapter 1 and fixed some typos for presentation.

--- auto/main.el
+++ auto/main.el
@@ -5,12 +5,12 @@
                      '(("ucsd" "12pt" "chapterheads")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("xy" "all") ("ulem" "normalem") ("caption" "font=small" "width=0.9\\textwidth") ("floatrow" "capposition=bottom") ("fontenc" "T1")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
